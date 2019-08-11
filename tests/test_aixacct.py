a49ea724428f475efd509e6a678edee24589ee40
Jackson Anderson <jda4923@rit.edu>
2019-08-10 21:18:13 -0400

    fix case errors

--- tests/test_aixacct.py
+++ tests/test_aixacct.py
@@ -15,7 +15,7 @@ def test_direct_load_aixACCT_hysteresis_data():
     assert all(elem in directFreqData for elem in RTfreqData)
 
 def test_direct_load_aixACCT_leakage_data():
-    RTlkgDir = join(sampledir, 'RTWHITEB_lkg')
+    RTlkgDir = join(sampledir, 'RTWhiteB_lkg')
     RTlkgFiles = hd.dir_read(RTlkgDir)
     lkglist = []
     for i in RTlkgFiles:
@@ -23,7 +23,7 @@ def test_direct_load_aixACCT_leakage_data():
         l.lcm_read(i)
         lkglist.append(l)
 
-    directreaddict = aix.read_tfdata(join(sampledir, 'RTWhiteB_lkg.dat'))
+    directreaddict = aix.read_tfdata(join(sampledir, 'RTWHITEB_lkg.dat'))
     directlkgData = aix.load_tfdata(directreaddict)
     print(all(elem in directlkgData for elem in lkglist))
     assert all(elem in directlkgData for elem in lkglist)
